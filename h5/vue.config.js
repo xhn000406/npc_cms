f9f3c2070d639496bbc0c3342970105ecbc385fe
fuzhu <1774880816@qq.com>
2022-04-26 16:10:42 +0800

    配置了mock启动的环境参数

--- h5/vue.config.js
+++ h5/vue.config.js
@@ -1,7 +1,7 @@
 const {
   defineConfig
 } = require('@vue/cli-service')
-const ENV_DEVELOPMENT = 'development'
+
 const ENV_PRODUCTION = 'production'
 const USE_MOCK = JSON.parse(process.env.USE_MOCK)
 const NODE_ENV = process.env.NODE_ENV
@@ -30,6 +30,7 @@ module.exports = defineConfig({
       // 使用mock构造假数据，在生产环境下 自动移除
       onBeforeSetupMiddleware: ({ app }) => {
         if (USE_MOCK) {
+          // 当前为生产环境的话就不启动了
           (NODE_ENV !== ENV_PRODUCTION) && require('./src/mock')(app)
         }
       }
